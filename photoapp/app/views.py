fdf35ed85800fee05f5312dbe8068334997e2e52
Alberto <albertiae.digital@gmail.com>
2022-12-24 13:57:23 -0400

    Included the categories in the views

--- photoapp/app/views.py
+++ photoapp/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 def gallery(request):
-    return render(request,'app/gallery.html')
+    categories = Category.objects.all()
+    context = {'categories':categories}
+    return render(request,'app/gallery.html',context)
 
 def view_photo(request,pk):
     return render(request,'app/photo.html')

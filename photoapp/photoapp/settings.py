83885a653e8fddc8352dc5b70bd8c4603a9f6b39
Alberto <albertiae.digital@gmail.com>
2022-12-24 13:28:36 -0400

    Added the static folder to the settings.py

--- photoapp/photoapp/settings.py
+++ photoapp/photoapp/settings.py
@@ -120,6 +120,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILE_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

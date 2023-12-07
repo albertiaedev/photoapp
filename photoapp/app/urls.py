2f61a514f5320b352db086e0a59904fcc35d20f8
jesusalberto18 <albertiae.digital@gmail.com>
2023-12-06 21:58:24 -0400

    Added urls to edit and delete templates, but still have to fix them

--- photoapp/app/urls.py
+++ photoapp/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.gallery,name='gallery'),
     path('photo/<str:pk>/',views.view_photo,name='photo'),
     path('add/',views.add_photo,name='add'),
+    path('delete/',views.delete_photo,name='delete'),
+    path('edit/',views.edit_photo,name='edit'),
 ]

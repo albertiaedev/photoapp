a7d1799adc3dc220afdb1f9903d1dcec1198890c
jesusalberto18 <albertiae.digital@gmail.com>
2023-12-07 12:25:33 -0400

    Added print statement in settings to show the route of templates

--- photoapp/photoapp/settings.py
+++ photoapp/photoapp/settings.py
@@ -136,3 +136,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print(os.path.join(BASE_DIR, 'templates'))
